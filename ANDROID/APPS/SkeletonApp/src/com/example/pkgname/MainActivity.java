9caad19bf7665a597516036e38941a97fccbb7a5
veganaiZe <veganaiZe@protonmail.com>
2023-07-07 09:09:04 -0700

    Add comment to lifecycle

--- ANDROID/APPS/SkeletonApp/src/com/example/pkgname/MainActivity.java
+++ ANDROID/APPS/SkeletonApp/src/com/example/pkgname/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends Activity
     @Override
     protected void onPause()
     {
-        super.onPause();
+        super.onPause();  // App will crash without super call.
     }
 
     /**
