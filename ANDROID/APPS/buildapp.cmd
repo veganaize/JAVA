0f169bbe2ffefd3f9c2c32d5f5cd3a9d14e81a77
veganaiZe <veganaiZe@protonmail.com>
2023-07-24 09:43:42 -0700

    Make batch file commands more consistent

--- ANDROID/APPS/buildapp.cmd
+++ ANDROID/APPS/buildapp.cmd
@@ -1,18 +1,21 @@
 @ECHO OFF
-@SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+@SETLOCAL ENABLEEXTENSIONS
 
 
 REM *TODO*:
-REM     Add assets folder to apk
-REM     Add libs folder to apk
-REM     Incremental compile --altered files only-- to improve speed
-REM     Migrate to PKCS12 keystore format? (or supress warning)
-REM     Compile Kotlin
-REM     Add a "clean" build task
-REM     Check and tune java target api levels
+REM     [ ] Add assets folder to apk
+REM     [ ] Add libs folder to apk
+REM     [ ] Incremental compile --altered files only-- to improve speed
+REM     [ ] Migrate to PKCS12 keystore format? (or supress warning)
+REM     [ ] Compile Kotlin
+REM     [ ] Add a "clean" build task
+REM     [ ] Check and tune java target api levels
+REM     [X] BUILD_DIR var
+REM     [X] Eliminate delayed var expansions?
 
-
-SET API_LEVEL=14
+SET MIN_API=14
+SET TARGET_API=14
+SET BUILD_DIR=compiled
 SET BUILD_TYPE=debug
 SET CMDLINE_TOOLS=8.0
 SET DEFAULT_ACTIVITY=MainActivity
@@ -30,7 +33,8 @@ IF "%*"=="--help" (
     @ECHO ---------
     @ECHO.
     @ECHO    ANDROID_HOME=^<root-path^>          - default: %ANDROID_HOME%
-    @ECHO    API_LEVEL=^<api-level^>             - default: %API_LEVEL%
+    @ECHO    MIN_API=^<api-level^>             - default: %MIN_API%
+    @ECHO    TARGET_API=^<api-level^>          - default: %TARGET_API%
     @ECHO    BUILD_TYPE=^<debug^|release^|clean^>  - default: %BUILD_TYPE%
     @ECHO    CMDLINE_TOOLS=^<version^|latest^>    - default: %CMDLINE_TOOLS%
     @ECHO    DEFAULT_ACTIVITY=^<class^>          - default: %DEFAULT_ACTIVITY%
@@ -45,7 +49,7 @@ IF "%*"=="--help" (
     @ECHO    gen\       - auto-generated R.java
     @ECHO    libs\      - .jar libraries
     @ECHO    assets\    - raw uncompiled assets
-    @ECHO    compiled\  - compiled artifacts
+    @ECHO    %BUILD_DIR%\  - compiled artifacts
     @ECHO.
 
     GOTO :EOF
@@ -64,9 +68,9 @@ IF NOT "%1"=="" (
 
 
 @ECHO Compiling resources...
-mkdir compiled 2>NUL
+mkdir %BUILD_DIR% 2>NUL
 aapt2 compile ^
-    -o compiled\res.zip ^
+    -o %BUILD_DIR%\res.zip ^
     --dir res
 IF NOT "%ERRORLEVEL%"=="0" GOTO :EOF
 
@@ -74,16 +78,16 @@ IF NOT "%ERRORLEVEL%"=="0" GOTO :EOF
 @ECHO Linking resources...
 mkdir gen 2>NUL
 aapt2 link ^
-    compiled\res.zip ^
-    -o compiled\unsigned.apk ^
+    %BUILD_DIR%\res.zip ^
+    -o %BUILD_DIR%\unsigned.apk ^
     --manifest AndroidManifest.xml ^
-    -I %ANDROID_HOME%\platforms\android-!API_LEVEL!\android.jar ^
+    -I %ANDROID_HOME%\platforms\android-%TARGET_API%\android.jar ^
     --java gen
 IF NOT "%ERRORLEVEL%"=="0" GOTO :EOF
 
 
 
-IF "!PACKAGE_NAME!"=="" (
+IF "%PACKAGE_NAME%"=="" (
     @ECHO Discovering package name...
     FOR /F %%r IN ('apkanalyzer manifest application-id compiled\unsigned.apk') DO (
         @ECHO     %%r
@@ -91,7 +95,7 @@ IF "!PACKAGE_NAME!"=="" (
     )
 ) ELSE (
     @ECHO Using provided package name...
-    @ECHO     !PACKAGE_NAME!
+    @ECHO     %PACKAGE_NAME%
 )
 
 
@@ -102,40 +106,34 @@ IF "!PACKAGE_NAME!"=="" (
 javac ^
     -source 1.7 ^
     -target 1.7 ^
-    -bootclasspath %ANDROID_HOME%\platforms\android-!API_LEVEL!\android.jar ^
+    -bootclasspath %ANDROID_HOME%\platforms\android-%TARGET_API%\android.jar ^
     -cp gen ^
-    -d compiled ^
+    -d %BUILD_DIR% ^
     src\%PACKAGE_NAME:.=\%\*.java
 IF NOT "%ERRORLEVEL%"=="0" GOTO :EOF
 
 
 @ECHO Dexxing classes...
-IF "%BUILD_TYPE%"=="release" (
-    set DEBUG=
-) ELSE (
-    set DEBUG=--debug
-)
-
-java -cp %ANDROID_HOME%\cmdline-tools\!CMDLINE_TOOLS!\lib\r8.jar com.android.tools.r8.D8 ^
-    %DEBUG% ^
-    --min-api !API_LEVEL! ^
-    --output compiled ^
-    --lib %ANDROID_HOME%\platforms\android-!API_LEVEL!\android.jar ^
+java -cp %ANDROID_HOME%\cmdline-tools\%CMDLINE_TOOLS%\lib\r8.jar com.android.tools.r8.D8 ^
+    --%BUILD_TYPE% ^
+    --min-api %MIN_API% ^
+    --output %BUILD_DIR% ^
+    --lib %ANDROID_HOME%\platforms\android-%TARGET_API%\android.jar ^
     compiled\%PACKAGE_NAME:.=\%\*.class
 IF NOT "%ERRORLEVEL%"=="0" GOTO :EOF
 
 
 @ECHO Adding dex to apk...
-jar Muf compiled\unsigned.apk -C compiled classes.dex
+jar Muf %BUILD_DIR%\unsigned.apk -C %BUILD_DIR% classes.dex
 IF NOT "%ERRORLEVEL%"=="0" GOTO :EOF
 
 
-IF EXIST compiled\debug.keystore (
+IF EXIST %BUILD_DIR%\debug.keystore (
     @ECHO Utilizing existing debug key...
 ) ELSE (
     @ECHO Generating debug key...
     keytool -genkey ^
-        -keystore compiled\debug.keystore ^
+        -keystore %BUILD_DIR%\debug.keystore ^
         -storepass android ^
         -alias androiddebugkey ^
         -keypass android ^
@@ -149,29 +147,29 @@ IF NOT "%ERRORLEVEL%"=="0" GOTO :EOF
 
 @ECHO Signing apk with debug key...
 call apksigner sign ^
-    --ks compiled\debug.keystore ^
+    --ks %BUILD_DIR%\debug.keystore ^
     --ks-pass pass:android ^
     --ks-key-alias androiddebugkey ^
-    --out compiled\signed.apk ^
-    compiled\unsigned.apk
+    --out %BUILD_DIR%\signed.apk ^
+    %BUILD_DIR%\unsigned.apk
 IF NOT "%ERRORLEVEL%"=="0" GOTO :EOF
 
 
 IF "%BUILD_TYPE%"=="release" (
     @ECHO Verifying apk signature...
-    call apksigner verify ^
-        --min-sdk-version !API_LEVEL! ^
-        compiled\signed.apk
+    call apksigner verify %BUILD_DIR%\signed.apk
     IF NOT "%ERRORLEVEL%"=="0" GOTO :EOF
 )
 
 
 IF "%BUILD_TYPE%"=="release" (
     @ECHO Recompressing apk with zopfli and aligning...
-    zipalign -zpf 4 compiled\signed.apk compiled\release.apk
+    zipalign -zpf 4 %BUILD_DIR%\signed.apk %BUILD_DIR%\release.apk
+    del debug.apk 2>NUL
 ) ELSE (
     @ECHO Aligning apk...
-    zipalign -p -f 4 compiled\signed.apk compiled\release.apk
+    zipalign -pf 4 %BUILD_DIR%\signed.apk %BUILD_DIR%\debug.apk
+    del release.apk 2>NUL
 )
 IF NOT "%ERRORLEVEL%"=="0" GOTO :EOF
 
